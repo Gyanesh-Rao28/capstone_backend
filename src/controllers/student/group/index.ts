c29066a0acf5b5c898e98eb4a8c4fed9b1a49704
Gyanesh Rao <gyaneshrao28@gmail.com>
2025-04-23 00:22:52 +0530

    student-group-fun

--- src/controllers/student/group/index.ts
+++ src/controllers/student/group/index.ts
@@ -11,8 +11,8 @@ export const createGroup = async (req: Request, res: Response) => {
         // const studentId = "258003e4-91de-4180-8906-a6d288bb540b";
         // const grpName = "NBytes";
 
-        const { projectId, studentId, grpName } = req.query;
-
+        const { projectId, grpName } = req.query;
+        const studentId = req.user?.student?.id
         // can extract studentId from req.user
 
         if(!projectId || !studentId || !grpName){
@@ -100,7 +100,7 @@ export const createGroup = async (req: Request, res: Response) => {
         console.log("stage 3: ", createGroupLeader)
 
 
-        res.status(200).json({
+        return res.status(200).json({
             status: 'success',
             data: { group, createGroupLeader }
         });
@@ -220,7 +220,7 @@ export const addGroupMember = async (req: Request, res: Response) => {
         console.log("Stage3: ", updatingGrpStatus)
 
 
-        res.status(200).json({
+        return res.status(200).json({
             status: 'success',
             data: updatingGrpStatus
         });
@@ -228,14 +228,218 @@ export const addGroupMember = async (req: Request, res: Response) => {
 
 
     } catch (error) {
-        console.error('Error creating project group:', error);
+        console.error('Error adding member:', error);
         return res.status(500).json({
             status: 'error',
-            message: 'Internal server error: creating group'
+            message: 'Internal server error: adding member'
         });
     }
 };
 
-// leaving project group
+// leaving project group [by member not by leader]
+export const leaveGroup = async (req: Request, res: Response) => {
+    try {
 
-// remove a member from project group
+        const {groupId} = req.query
+        const studentId = req.user?.student?.id
+
+        if (!studentId) {
+            res.status(401).json({
+                status: "Unauthorized",
+                message: "Invalid Student Id"
+            })
+            return;
+        }
+        if (!groupId){
+            res.status(404).json({
+                status: "Not found",
+                message: "Group Id required"
+            })
+            return;
+        }
+
+        const isGroupExist = await prisma.group.findFirst({
+            where:{
+                id: groupId as string
+            }
+        })
+
+        console.log("stage 0:", isGroupExist)
+
+        if (!isGroupExist){
+            res.status(404).json({
+                status: "Not found",
+                message: "Group doesn't exist or invalid group id"
+            })
+            return
+        }
+
+        const isMember = await prisma.member.findFirst({
+            where:{
+                groupId: groupId as string,
+                studentId
+            }
+        })
+
+        if(!isMember){
+            res.status(403).json({
+                status: "Forbidden",
+                message: "your are not a member of this group"
+            })
+            return
+        }
+
+        console.log("stage 1:", isMember)
+
+        if(isMember?.memberRole === MemberRole.Leader){
+            res.status(409).json({
+                status: "Conflict",
+                message: "Group leader can't leave the group"
+            })
+            return
+        }
+
+        const removeMember = await prisma.member.delete({
+            where:{
+                id: isMember?.id
+            }
+        })
+
+        if(!removeMember){
+            res.status(409).json({
+                status: "Conflict",
+                message: "Group leader can't leave the group"
+            })
+            return
+        }
+
+        let updateMember = Number( isGroupExist?.currentMember) - 1
+
+        const updatedGroup = await prisma.group.update({
+            where:{
+                id: isGroupExist?.id
+            },
+            data:{
+                currentMember: updateMember,
+                isOpen: true
+            }
+        })
+
+
+        return res.status(200).json({
+            status: 'success',
+            data: updatedGroup
+        });
+
+
+    } catch (error) {
+        console.error('Error adding member:', error);
+        return res.status(500).json({
+            status: 'error',
+            message: 'Internal server error: adding member'
+        });
+    }
+};
+
+// remove a member from project group [it will done by leader only]
+export const removeMember = async (req: Request, res: Response) => {
+    try {
+        // here memberId means - which leader wants to remove member from group
+        const { memberId, groupId } = req.query
+        const studentId = req.user?.student?.id
+
+        if (!studentId) {
+            res.status(401).json({
+                status: "Unauthorized",
+                message: "Invalid Student Id"
+            })
+            return;
+        }
+        if (!memberId || !groupId) {
+            res.status(404).json({
+                status: "Not found",
+                message: "all parameters required"
+            })
+            return;
+        }
+
+        const isGroupExist = await prisma.group.findFirst({
+            where:{
+                id: groupId as string
+            }
+        })
+
+        
+        const isLeader = await prisma.member.findFirst({
+            where:{
+                studentId: studentId,
+                groupId: groupId as string
+            }
+        })
+
+        if(!isLeader){
+            res.status(409).json({
+                status: "Conflict",
+                message: "You are not a part of this group"
+            })
+            return;
+        }
+
+
+        if(isLeader?.memberRole !== MemberRole.Leader){
+            res.status(409).json({
+                status: "Conflict",
+                message: "Only Group Leader can remove the group"
+            })
+            return;
+        }
+
+        const isRemovingMemberExist = await prisma.member.findFirst({
+            where:{
+                id: memberId as string
+            }
+        })
+        
+        if(isLeader.groupId !== isRemovingMemberExist?.groupId){
+            res.status(409).json({
+                status: "Conflict",
+                message: "This member doesn't belong to your group"
+            })
+            return;
+        }
+
+        await prisma.member.delete({
+            where:{
+                id: isRemovingMemberExist.id
+            }
+        })
+
+
+        let updateMember = Number(isGroupExist?.currentMember) - 1
+
+        const updatedGroup = await prisma.group.update({
+            where: {
+                id: isGroupExist?.id
+            },
+            data: {
+                currentMember: updateMember,
+                isOpen: true
+            }
+        })
+        
+
+
+        return res.status(200).json({
+            status: 'success',
+            data: updatedGroup
+        });
+
+
+    } catch (error) {
+        console.error('Error adding member:', error);
+        return res.status(500).json({
+            status: 'error',
+            message: 'Internal server error: adding member'
+        });
+    }
+};
--- src/routes/student.ts
+++ src/routes/student.ts
@@ -1,7 +1,7 @@
 import { Router, RequestHandler } from "express";
 
 import { getStudent } from "../controllers/student/student";
-import { createGroup, addGroupMember } from "../controllers/student/group";
+import { createGroup, addGroupMember, leaveGroup, removeMember } from "../controllers/student/group";
 
 
 const studentRouter: Router = Router();
@@ -13,5 +13,9 @@ studentRouter.get('/', getStudent as RequestHandler);
 
 studentRouter.post('/createGroup', createGroup as RequestHandler);
 studentRouter.put('/addGroupMember', addGroupMember as RequestHandler);
+studentRouter.delete('/leaveGroup', leaveGroup as RequestHandler);
+studentRouter.delete('/removeMember', removeMember as RequestHandler);
+
+// create project application
 
 export default studentRouter;
